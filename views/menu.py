e25148fd7c76d7f5508751e1f65ad72356321bfc
Mya1212 <89876659+Mya1212@users.noreply.github.com>
2022-06-09 10:45:31 +0200

    Add files via upload

---
+++ views/menu.py
@@ -0,0 +1,22 @@
+class menu_views:
+
+    def __init__(self):
+        pass
+
+    def display_main_menu(self):
+        print("\n\n=== MAIN MENU ===\n")
+        print("[1] Tournament menu")
+        print("[2] Player menu")
+        print("[3] Exit program")
+
+    def display_tournament_menu(self):
+        print("\n\n=== TOURNAMENT MENU ===\n")
+        print("[1] Tournament list")
+        print("[2] Create tournanment")
+        print("[3] Return main menu")
+
+    def display_player_menu(self):
+        print("\n\n=== PLAYER MENU ===\n")
+        print("[1] Players list")
+        print("[2] Create player")
+        print("[3] Return main menu")
