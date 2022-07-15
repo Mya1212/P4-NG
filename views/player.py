f6e442c5a9f4722583416d84fe85fc7bad8beb18
Mya1212 <89876659+Mya1212@users.noreply.github.com>
2022-07-15 14:49:31 +0200

    Add files via upload

--- views/player.py
+++ views/player.py
@@ -1,15 +1,19 @@
 from controlers.player import Player
 
-class Create_player:
+class PlayerViews:
 
-    first_name = input('Entrez votre nom :\n>')
-    last_name = input('Entrez votre prénom :\n>')
-    birthday = input ('Entrez votre date de naissance XX/XX/XXXX:\n>')
-    sex = input ('Entrez votre genre F ou M:\n>')
-    rank = input ('Entrez votre classement:\n>')
+    def create_Player(self):
 
-    def ask_gender(self) -> str:
+        first_name = input('Entrez votre nom :\n>')
+        last_name = input('Entrez votre prénom :\n>')
+        birthday = input ('Entrez votre date de naissance XX/XX/XXXX:\n>')
+        sex = input ('Entrez votre genre F ou M:\n>')
+        rank = input ('Entrez votre classement:\n>')
+
         if sex == "F" or sex == "M":
             return sex
         else:
-            print("Erreur veuillez entrez un genre valide F ou Md")
+            print("Erreur veuillez entrez un genre valide F ou M")
+
+    def display_player_list(self):
+        pass
