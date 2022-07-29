62991bb45468234820d13feb03b2fad6c750205b
Mya1212 <89876659+Mya1212@users.noreply.github.com>
2022-07-29 15:10:57 +0200

    Add files via upload

--- models/player.py
+++ models/player.py
@@ -1,15 +1,65 @@
-from controlers.player import Player
+from views.player import PlayerViews
+from views.menu import MenuViews
 
-class CreatePlayer:
+class PlayerController:
 
-    first_name = input('Entrez votre nom :\n>')
-    last_name = input('Entrez votre prénom :\n>')
-    birthday = input ('Entrez votre date de naissance XX/XX/XXXX:\n>')
-    sex = input ('Entrez votre genre F ou M:\n>')
-    rank = input ('Entrez votre classement:\n>')
+    def update_player(self):
+        while True:
+            self.menu.display_player_menu()
+            user_input=input()
 
-    def ask_gender(self) -> str:
-        if sex == "F" or sex == "M":
-            return sex
-        else:
-            print("Erreur veuillez entrez un genre valide F ou M")
+            if user_input=="1":
+                #player list
+                #select player
+                #edit
+                pass
+            
+            elif user_input=="2":
+                #player list
+                #select player
+                #delete
+                pass
+
+            elif user_input=="3":
+                self.menu.display_player_menu()
+            
+            else:
+                self.menu.display_message("Invalid choice")
+
+    def display_player_name(self):
+        #players = []
+        #players.sort(key=)
+        self.views.diplay_player_list()
+
+    def display_player_rank(self, sort_by: str):
+        self.views.diplay_player_list()
+        
+    def menu_player(self):
+        while True:
+            self.menu.display_player_menu()
+            user_input=input()
+
+            if user_input=="1":
+                self.create_player()
+            
+            elif user_input=="2":
+                self.update_player()
+
+            elif user_input=="3":
+                self.display_player_name()
+
+            elif user_input=="4":
+                self.display_player_rank()
+
+            elif user_input=="5":
+                self.menu.display_player_menu()
+                exit()
+            
+            else:
+                self.menu.display_message("Invalid choice")
+
+if __name__ == "__main__":
+    player = Player("pierre","dupont","12-12-1988","M","800")
+    print(player)
+    data = player.serialize()
+    print(data)
